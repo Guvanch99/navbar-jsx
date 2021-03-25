c9fcdb1d8f31910a3017602acb6c02ba6528839d
Guvanch Avediyev <awediyewguwanc@gmail.com>
2021-03-25 15:32:39 -0700

    navbar commit

---
+++ src/Navbar.js
@@ -0,0 +1,55 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { FaBars, FaTwitter } from 'react-icons/fa'
+import { links, social } from './data'
+import logo from './logo.svg'
+
+const Navbar = () => {
+  const [state, setstate] = useState(false)
+  const lRef = useRef()
+  const cref = useRef()
+  useEffect(() => {
+    const linksHeight = lRef.current.getBoundingClientRect().height;
+    if (state) {
+      cref.current.style.height = `${linksHeight}px`
+    } else {
+      cref.current.style.height = `0px`
+    }
+  }, [state])
+  return (
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} alt="logo" />
+        <button onClick={setstate(!state)} className="nav-toggle">
+          <FaBars />
+        </button>
+      </div>
+
+      <div className='linkd-container' ref={cref}>
+        <ul className="links" ref={lRef}>
+          {links.map((link) => {
+            const { id, url, text } = link
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+
+
+      <ul className="social-icons">
+        {social.map((link) => {
+          const { id, url, icon } = link
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
